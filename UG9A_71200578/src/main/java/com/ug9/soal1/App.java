7eacd61a9ced6259a839d52c659cdd15fcf481a8
Felix-71200578 <felix.nathanael@ti.ukdw.ac.id>
2022-04-20 18:47:39 +0700

    UG9A_71200578

---
+++ UG9A_71200578/src/main/java/com/ug9/soal1/App.java
@@ -0,0 +1,22 @@
+package com.ug9.soal1;
+
+
+public class App 
+{
+    public static void main( String[] args ) {
+        Lina lina = new Lina();
+        DragonKnight dragonKnight = new DragonKnight();
+        LegionCommander legionCommander = new LegionCommander();
+
+        lina.skill(dragonKnight);
+        dragonKnight.skill();
+        dragonKnight.attack(lina);
+        dragonKnight.attack(lina);
+        legionCommander.skill(lina);
+        lina.skill(legionCommander);
+
+        lina.showCharacterInfo();
+        dragonKnight.showCharacterInfo();
+        legionCommander.showCharacterInfo();
+    }
+}
